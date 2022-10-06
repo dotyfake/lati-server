fd256e23a31d86f88db3c93873c6d3c48a894a3f
Doty <douthanh97@gmail.com>
2022-10-07 06:44:43 +0700

    add cors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
  const authRouter = require('./auth')
  const gamesRouter = require('./games')
+ const cors = require('cors')
+
+
 function route (app){
+    app.use(cors())
+
     app.use('/api/auth', authRouter)
     
     app.use('/api/games', gamesRouter)
