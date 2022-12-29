18b2a7c878f27ba39f8f8ef39e22e503d5296412
Doty <douthanh97@gmail.com>
2022-12-30 01:12:30 +0700

    update method socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,13 +1,13 @@
 const socketIo = (io) => {
-    let activeUsers = [];
-
+  let activeUsers = [];
+  let rooms = []
 io.on("connection", (socket) => {
   // add new User
   socket.on("new-user-add", (newUserId) => {
     // if user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
-      console.log("New User Connected", activeUsers);
+      // console.log("New User Connected", activeUsers);
     }
     // send all active users to new user
     io.emit("get-users", activeUsers);
@@ -16,26 +16,23 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    console.log("User Disconnected", activeUsers);
+    // console.log("User Disconnected", activeUsers);
+    rooms = rooms.filter((room) => socket.id !== room.socketId)
     // send all active users to all users
     io.emit("get-users", activeUsers);
   });
 
-  //join room chat
-  socket.on("join-room", (socketId) => {
-    socket.join(socketId);
+  socket.on("join-room", (userId) => {
+    rooms.push({userId, socketId: socket.id})
   })
 
   // send message to a specific user
   socket.on("send-message", (data) => {
     const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("activeUsers:", activeUsers);
-    console.log("Sending from socket to :", receiverId)
-    console.log("Data: ", data)
-    console.log("user:", user);
-    if (user) {
-      io.to(user.socketId).emit("receive-message", data);
+    const room = rooms.find((r) => r.userId === receiverId);
+    // console.log("Data: ", data , "room:", rooms)
+    if (room) {
+      io.to(room.socketId).emit("receive-message", data);
     }
   });
 });
